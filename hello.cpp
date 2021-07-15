4b3b5ece6726538cae4cc28462b80e379d152732
Adachi <81370812+HayasakaI@users.noreply.github.com>
2021-07-15 09:02:11 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    cout << "change the file" << endl;
+    cout << "在远程上修改" << endl;
     cout << "now change it again." << endl;
     cout << "Hello" << endl;
     return 0;
