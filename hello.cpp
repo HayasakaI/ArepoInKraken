a88095b0e05652d2ebde2c65b08f843e5d0ffb32
Hemo <1120510004@qq.com>
2021-07-15 09:16:17 +0800

    再在本地代码中增加文字，1

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    cout << "在远程上修改代码" << endl;
+    cout << "在本地上修改代码，然后加点东西" << endl;
     cout << "now change it again." << endl;
     cout << "Hello" << endl;
     return 0;
