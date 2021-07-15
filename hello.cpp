7885be482015d69c290a50c7499620b93b7b916c
Adachi <81370812+HayasakaI@users.noreply.github.com>
2021-07-15 09:17:27 +0800

    Update hello.cpp
    在远程修改代码，2

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    cout << "在远程上修改代码" << endl;
+    cout << "加点东西，在远程上修改代码" << endl;
     cout << "now change it again." << endl;
     cout << "Hello" << endl;
     return 0;
